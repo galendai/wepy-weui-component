6c2736efa8428c978333b7aa65d95926711c6927
Galen Dai <galen@leanwing.com>
2017-05-24 22:14:43 +0800

    fix some eslint error

--- wepy.config.js
+++ wepy.config.js
@@ -2,50 +2,50 @@ const path = require('path');
 let prod = process.env.NODE_ENV === 'production';
 
 module.exports = {
-  eslint: false,
-  wpyExt: ".wpy",
-  build: {
-    web: {
-      htmlTemplate: path.join('src', 'index.template.html'),
-      htmlOutput: path.join('web', 'index.html'),
-      jsOutput: path.join('web', 'index.js')
-    }
-  },
-  compilers: {
-    sass: {
-      outputStyle: "compressed"
+    eslint: false,
+    wpyExt: '.wpy',
+    build: {
+        web: {
+            htmlTemplate: path.join('src', 'index.template.html'),
+            htmlOutput: path.join('web', 'index.html'),
+            jsOutput: path.join('web', 'index.js')
+        }
     },
-    babel: {
-      sourceMap: true,
-      presets: [
-        "es2015",
-        "stage-1"
-      ],
-      plugins: [
-        "transform-export-extensions",
-        "syntax-export-extensions",
-      ]
+    compilers: {
+        sass: {
+            outputStyle: 'compressed'
+        },
+        babel: {
+            sourceMap: true,
+            presets: [
+                'es2015',
+                'stage-1'
+            ],
+            plugins: [
+                'transform-export-extensions',
+                'syntax-export-extensions',
+            ]
+        }
     }
-  }
 };
 
 
 if (prod) {
 
-  delete module.exports.compilers.babel.sourcesMap;
-  // 压缩sass
-  // module.exports.compilers['sass'] = {outputStyle: 'compressed'}
+    delete module.exports.compilers.babel.sourcesMap;
+    // 压缩sass
+    // module.exports.compilers['sass'] = {outputStyle: 'compressed'}
 
-  // 压缩less
-  module.exports.compilers['less'] = {compress: true}
+    // 压缩less
+    module.exports.compilers['less'] = {compress: true};
 
-  // 压缩js
-  module.exports.plugins = {
-    uglifyjs: {
-      filter: /\.js$/,
-      config: {
-      }
-    },
+    // 压缩js
+    module.exports.plugins = {
+        uglifyjs: {
+            filter: /\.js$/,
+            config: {
+            }
+        },
     /*imagemin: {
       filter: /\.(jpg|png|jpge)$/,
       config: {
@@ -57,6 +57,5 @@ if (prod) {
         }
       }
     }*/
-  }
+    };
 }
-
